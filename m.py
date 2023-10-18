d42e6ef1e21e2e9692b79912f4d802def8aa6f31
shelzyxpython <144206667+shelzyxpython@users.noreply.github.com>
2023-10-18 17:21:42 +0200

    Add files via upload

---
+++ m.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# x = np.linspace(0, 10, 50)
+# y = 6*x - 2
+#
+# plt.title('График y = 6x - 2')
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.plot(x,y,'b-*')
+# plt.grid()
+# plt.show()
+
+# x = np.linspace(0, 10, 50)
+# y1 = 6*x**3-2*x + 8
+# y2 = 3*x + 1
+#
+# plt.title('График F(x,y)')
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.plot(x,y1, 'r--',x,y2, 'b--')
+# plt.grid()
+# plt.show()
+
+# product = ["Помидоры","Огурцы","Тыква","Свекла","Редис","Картофель"]
+# quantity = [100, 65, 12, 47, 89, 256]
+# plt.bar(product, quantity)
+# plt.title('Товар')
+# plt.ylabel("Количество")
+# plt.show()
+
+import fuzzywuzzy as fzw
+import calendar
